e3d8bd8637df6d2d8b0a7f6324a6748167225ef4
Anand Bose <anandbose16@gmail.com>
2025-05-17 23:02:07 +0530

    Update security filter to use OAuth2 authorization server implemented in "todo-authorization-server" project

--- src/main/java/dev/anandbose/todoapp/TodoSecurityConfig.java
+++ src/main/java/dev/anandbose/todoapp/TodoSecurityConfig.java
@@ -2,14 +2,9 @@ package dev.anandbose.todoapp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,20 +12,25 @@ public class TodoSecurityConfig {
 
 	@Bean
 	public SecurityFilterChain appSecurity(HttpSecurity httpSecurity) throws Exception {
-		return httpSecurity.httpBasic(Customizer.withDefaults()).authorizeHttpRequests(http -> {
-			http.anyRequest().authenticated();
-		}).csrf(csrf -> csrf.disable()).build();
+		return httpSecurity.authorizeHttpRequests(http -> {
+			http.requestMatchers(HttpMethod.GET, "/todo/**").hasAuthority("SCOPE_todo.read")
+			.requestMatchers(HttpMethod.POST, "/todo/**").hasAuthority("SCOPE_todo.write")
+			.requestMatchers(HttpMethod.PATCH, "/todo/**").hasAuthority("SCOPE_todo.write")
+			.requestMatchers(HttpMethod.DELETE, "/todo/**").hasAuthority("SCOPE_todo.write");
+		})
+		.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+		.build();
 	}
 
-	@Bean
-	public UserDetailsManager userDetailsManager(PasswordEncoder passwordEncoder) {
-		UserDetails anand = User.withUsername("anand").password(passwordEncoder.encode("abc123")).build();
-		UserDetails meera = User.withUsername("meera").password(passwordEncoder.encode("xyz1234")).build();
-		return new InMemoryUserDetailsManager(anand, meera);
-	}
+	// @Bean
+	// public UserDetailsManager userDetailsManager(PasswordEncoder passwordEncoder) {
+	// 	UserDetails anand = User.withUsername("anand").password(passwordEncoder.encode("abc123")).build();
+	// 	UserDetails meera = User.withUsername("meera").password(passwordEncoder.encode("xyz1234")).build();
+	// 	return new InMemoryUserDetailsManager(anand, meera);
+	// }
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-	}
+	// @Bean
+	// public PasswordEncoder passwordEncoder() {
+	// 	return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	// }
 }
